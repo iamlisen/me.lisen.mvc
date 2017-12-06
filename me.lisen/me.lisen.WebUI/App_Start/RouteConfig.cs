896851a4c42e28de1ba0fe5bde93bf1a2aac347a
李森 <lisen@lisen.me>
2017-12-06 15:08:48 +0800

    area

--- me.lisen/me.lisen.WebUI/App_Start/RouteConfig.cs
+++ me.lisen/me.lisen.WebUI/App_Start/RouteConfig.cs
@@ -14,10 +14,11 @@ namespace me.lisen.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-     name: "Default",
-     url: "{controller}/{action}/{id}",
-     defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
- );
+             name: "Default",
+             url: "{controller}/{action}/{id}",
+             defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+             namespaces: new[] { "me.lisen.WebUI.Controllers" }
+             );
         }
     }
 }
